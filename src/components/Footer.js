e587b680456581df126a52e7ac2a3d18c787be51
Harithmech <harithmech@gmail.com>
2024-05-10 09:23:09 +0530

    made changes to fotter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ export default function Footer() {
         <footer className="max-w-6xl mx-auto bg-white py-4 px-6 flex overflow-x-auto justify-between items-center ">
             <div className="text-slate-600 whitespace-nowrap">where every page turns into a new adventure</div>
             <div className="flex space-x-2 text-slate-500">
-                <a href="https://x.com/neilshroff" target="_blank" rel="noopener noreferrer">@mechboyharith</a>,
+                <a href="https://twitter.com/ybhrdwj" target="_blank" rel="noopener noreferrer">@yash</a>
+                <a href="https://twitter.com/harith_m1999" target="_blank" rel="noopener noreferrer">@harith</a>
             </div>
         </footer>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@
 export default function Header() {
     return (
       <header className=" bg-white max-w-6xl mx-auto py-4 px-6">
-        <div className="text-xl text-slate-700 font-bold flex justify-center items-center">📚PagesOnFire🔥</div>
+        <div className="text-xl text-slate-700 font-bold flex justify-center items-center">📚📜PagesandPioneer🚀🔥</div>
       </header>
     );
   }
