b6a2da3b64d7e39d6357c6b8fa997942cbe4b083
Harithmech <harithmech@gmail.com>
2024-05-15 21:38:19 +0530

    Updated meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
       <meta property="twitter:description" content="The Ultimate Book Lover's Guide: Discovering Your Next Read, Ideal Nooks, and Literary Adventures"></meta>
       <meta property="description" content="The Ultimate Book Lover's Guide: Discovering Your Next Read, Ideal Nooks, and Literary Adventures" />
       <meta property="og:image" content="https://www.pagesandpioneer.com/opengraph.png"></meta>
-      <meta property="og:title" content="Pondy Life"></meta>
+      <meta property="og:title" content="Pages and Poineer"></meta>
       <meta property="og:description" content="The Ultimate Book Lover's Guide: Discovering Your Next Read, Ideal Nooks, and Literary Adventures"/>
       <meta property="og:url" content="pagesandpoineer.com"></meta> </Head>
       <body>
